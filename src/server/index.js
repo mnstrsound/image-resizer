748868d9d26bdffda088f0f7ab46337e0c409629
mnstrsound <mnstrsound@gmail.com>
2017-10-22 21:18:51 +0300

    fixes

--- src/server/index.js
+++ src/server/index.js
@@ -7,13 +7,15 @@ import { routes, allowedMethods } from './routes';
 
 const app = new Koa();
 
+const env = process.env.NODE_ENV || 'development';
+
 app.use(pug('src/server/views'));
 app.use(koaBody(koaBodyConfig));
 app.use(routes());
 app.use(allowedMethods());
 
 app.use((ctx) => {
-    ctx.render('index', { state: JSON.stringify({}) });
+    ctx.render('index', { state: JSON.stringify({}), env });
 });
 
 app.listen(3000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const path = require('path');
 const ARUI_TEMPLATE = require('arui-presets/webpack.base');
 const ARUI_DEV_TEMPLATE = require('arui-presets/webpack.development');
 const ARUI_PROD_TEMPLATE = require('arui-presets/webpack.production');
 const merge = require('webpack-merge');
 
 module.exports = merge.smart(
-    { entry: './src/index.jsx' },
+    {
+        entry: './src/index.jsx',
+        output: {
+            path: path.resolve(__dirname, './build/'),
+            filename: '[name].js'
+        }
+    },
     ARUI_TEMPLATE,
     process.env.NODE_ENV === 'production' ? ARUI_PROD_TEMPLATE : ARUI_DEV_TEMPLATE
 );
